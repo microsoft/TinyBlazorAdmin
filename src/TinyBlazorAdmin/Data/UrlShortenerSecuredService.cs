9dfaed192f9d989578989cf55f909bc4848140ac
Frank <fboucher@outlook.com>
2020-07-15 11:07:19 -0400

    login is still failling

--- src/TinyBlazorAdmin/Data/UrlShortenerSecuredService.cs
+++ src/TinyBlazorAdmin/Data/UrlShortenerSecuredService.cs
@@ -1,10 +1,8 @@
-using System;
+
 using System.Net.Http;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
-using TinyBlazorAdmin.Data;
 
 namespace TinyBlazorAdmin.Data
 {
