936e5ef345e430448fc5eaad967b41f79ff0b5da
fboucher <fboucher@outlook.com>
2022-11-16 20:36:09 -0500

    Stats are working!

--- src/TinyBlazorAdmin/Program.cs
+++ src/TinyBlazorAdmin/Program.cs
@@ -14,6 +14,8 @@ builder.Services.AddMsalAuthentication(options =>
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
 });
 // regiser fusion blazor service
+// Community Licence for your personal use ONLY. Thank you Syncfusion for this generous offer.
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NzYyMzI1QDMyMzAyZTMxMmUzMFY0cEZ3MVozdkwvekVhek8xTWdPMkg2NlhvdVFNR1lvZHdhQWJWUlNjZW89"); 
 builder.Services.AddSyncfusionBlazor(options => { options.IgnoreScriptIsolation = true; });
 
 await builder.Build().RunAsync();
