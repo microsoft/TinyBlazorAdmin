e4b18772d4e11056534cec38da5d8f48ac24c4ad
fboucher <fboucher@outlook.com>
2022-11-24 21:49:19 +0000

    removing prefix

--- src/client/Program.cs
+++ src/client/Program.cs
@@ -9,7 +9,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services
-        .AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) })
+        .AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
         .AddStaticWebAppsAuthentication();
 
 // builder.Services.AddMsalAuthentication(options =>
